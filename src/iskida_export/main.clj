4da790faf77cc4c239b52eba12e2345c162753f6
saidone <saidone@saidone.org>
2020-03-18 18:38:02 +0100

    removed categories/tags

--- src/iskida_export/main.clj
+++ src/iskida_export/main.clj
@@ -3,13 +3,11 @@
 
 (require '[iskida-export.config :as config]
          '[iskida-export.users :as users]
-         '[iskida-export.categories :as categories]
          '[iskida-export.pages :as pages]
          '[iskida-export.comments :as comments])
 
 (defn -main [& args]
   (users/gen-users)
-  (categories/gen-categories)
   (def xml (pages/gen-pages))
   (comments/gen-comments xml))
 
