8dd0ec869ae88d9e44acda355591414adca6bd55
saidone <saidone@saidone.org>
2020-03-25 12:21:13 +0100

    external config file

--- src/iskida_export/config.clj
+++ src/iskida_export/config.clj
@@ -1,26 +1,32 @@
 (ns iskida-export.config
+  (:require [immuconf.config :as immu])
   (:gen-class))
+
+(def config
+  (immu/load "resources/config_riusa.edn"))
 
 ;; list of page files
 (def page-files
   (filter
    #(re-matches #".*\.ffc" (.getName %))
-   (file-seq (clojure.java.io/file "/home/saidone/workspace-clojure/iskida-export/resources/users_riusa/news"))))
+   (file-seq (clojure.java.io/file (immu/get config :page-dir)))))
 
 ;; list of image files
 (def image-files
   (filter
    #(re-matches #".*\.(jpg|jpeg|gif|png)" (.getName %))
-   (file-seq (clojure.java.io/file "/home/saidone/workspace-clojure/iskida-export/resources/users_riusa/images"))))
+   (file-seq (clojure.java.io/file (immu/get config :image-dir)))))
 
 ;; list of comment files
 (def comments-files
   (filter
    #(re-matches #"^news.*\.ffc" (.getName %))
-   (file-seq (clojure.java.io/file "/home/saidone/workspace-clojure/iskida-export/resources/users_riusa/comments"))))
+   (file-seq (clojure.java.io/file (immu/get config :comment-dir)))))
 
 ;; users CSV
-(def users-csv (slurp "/home/saidone/workspace-clojure/iskida-export/resources/users_riusa.csv"))
+(def users-csv
+  (slurp (immu/get config :users-csv)))
 
 ;; fixed users CSV
-(def fixed-users-csv (slurp "/home/saidone/workspace-clojure/iskida-export/resources/fixed_users_riusa.csv"))
+(def fixed-users-csv
+  (slurp (immu/get config :fixed-users-csv)))
