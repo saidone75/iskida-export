eeb464a366e47d138a4c72532f678c80a218bca6
saidone <saidone@saidone.org>
2020-03-26 11:50:53 +0100

    keep old APIs with immuconf

--- src/iskida_export/config.clj
+++ src/iskida_export/config.clj
@@ -2,31 +2,67 @@
   (:require [immuconf.config :as immu])
   (:gen-class))
 
-(def config
-  (immu/load "resources/config_riusa.edn"))
-
 ;; list of page files
-(def page-files
-  (filter
-   #(re-matches #".*\.ffc" (.getName %))
-   (file-seq (clojure.java.io/file (immu/get config :page-dir)))))
+(def page-files)
 
 ;; list of image files
-(def image-files
-  (filter
-   #(re-matches #".*\.(jpg|jpeg|gif|png)" (.getName %))
-   (file-seq (clojure.java.io/file (immu/get config :image-dir)))))
+(def image-files)
 
 ;; list of comment files
-(def comments-files
-  (filter
-   #(re-matches #"^news.*\.ffc" (.getName %))
-   (file-seq (clojure.java.io/file (immu/get config :comment-dir)))))
+(def comment-files)
 
 ;; users CSV
-(def users-csv
-  (slurp (immu/get config :users-csv)))
+(def users-csv)
 
 ;; fixed users CSV
-(def fixed-users-csv
-  (slurp (immu/get config :fixed-users-csv)))
+(def fixed-users-csv)
+
+;; XML output file for pages
+(def pages-xml-output)
+
+;; SQL output file for comments
+(def comments-sql-output)
+
+;; XML output file for users
+(def users-xml-output)
+
+(defn make-config [config]
+  (alter-var-root
+   #'page-files
+   (constantly
+    (filter
+     #(re-matches #".*\.ffc" (.getName %))
+     (file-seq (clojure.java.io/file (immu/get config :pages-dir))))))
+  (alter-var-root
+   #'image-files
+   (constantly
+    (filter
+     #(re-matches #".*\.(jpg|jpeg|gif|png)" (.getName %))
+     (file-seq (clojure.java.io/file (immu/get config :images-dir))))))
+  (alter-var-root
+   #'comment-files
+   (constantly
+    (filter
+     #(re-matches #"^news.*\.ffc" (.getName %))
+     (file-seq (clojure.java.io/file (immu/get config :comments-dir))))))
+  (alter-var-root
+   #'users-csv
+   (constantly
+    (slurp (immu/get config :users-csv))))
+  (alter-var-root
+   #'fixed-users-csv
+   (constantly
+    (slurp (immu/get config :fixed-users-csv))))
+  (alter-var-root
+   #'pages-xml-output
+   (constantly
+    (immu/get config :pages-xml-output)))
+  (alter-var-root
+   #'comments-sql-output
+   (constantly
+    (immu/get config :comments-sql-output)))
+  (alter-var-root
+   #'users-xml-output
+   (constantly
+    (immu/get config :users-xml-output)))
+  nil)
