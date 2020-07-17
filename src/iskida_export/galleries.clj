9e4263560d76cf35688869374c64b118ab83d9cb
saidone <saidone@saidone.org>
2020-07-17 10:37:43 +0200

    fix owner length, lft and rgt values, copy images also in original

--- src/iskida_export/galleries.clj
+++ src/iskida_export/galleries.clj
@@ -10,7 +10,6 @@
          '[iskida-export.users :as users]
          '[iskida-export.utils :as utils])
 
-
 (def gallery-id (atom 1))
 (defn- get-gallery-id []
   (swap! gallery-id inc))
@@ -18,6 +17,13 @@
 (def image-id (atom 1))
 (defn- get-image-id []
   (swap! image-id inc))
+
+(def lft-id (atom 1))
+(defn- get-lft-id []
+  (swap! lft-id inc))
+
+(defn- get-rgt-id []
+  (swap! lft-id inc))
 
 (def asset-id (atom 9000))
 (defn- get-asset-id []
@@ -27,38 +33,43 @@
 (defn- get-image-ordering []
   (swap! image-ordering inc))
 
+(defn filter-owner [owner]
+  (if owner
+    (-> owner
+        (s/trim)
+        (s/replace #"," "")
+        (s/replace #"^(partners|fakeaccounts|accounts|staff)\." "")
+        (s/replace #"@.*$" ""))
+    ""))
+
 (defn parse-gallery [galleries gallery-file]
   (let [gallery-map (parser/ffc-map  (slurp gallery-file))]
     (assoc
      galleries
      (get-gallery-id)
      {:title (:title gallery-map)
-      :content (map #(s/replace % #"^images\." "") (s/split (:gallery gallery-map) #","))})))
+      :content (map #(s/replace % #"^[ ]*images\." "") (s/split (:gallery gallery-map) #","))})))
 
 (defn parse-image-ffc [image-name]
   (parser/ffc-map
    (slurp (str "resources/users_veramente/images/" image-name ".ffc"))))
 
-;;test
-(def gal (first
-          (reduce
-           parse-gallery
-           {}
-           config/galleries)))
-
 (defn create-image [path gallery-id image-name]
+  (println (str path " " gallery-id " " image-name))
   (let [image-map (parse-image-ffc image-name)]
-    (spit "/home/saidone/Vagrant VMs/veramente-deb/copy-images.sh" (str "cp '" image-name "&main.jpg' " "joomgallery/details/" path "/" image-name ".jpg" "\n") :append true)
-    (spit "/home/saidone/Vagrant VMs/veramente-deb/copy-images.sh" (str "mogrify -path joomgallery/thumbnails/" path " -thumbnail 160x160 joomgallery/details/" path "/" image-name ".jpg" "\n") :append true)
+    (spit config/galleries-sh (str "#'"image-name "&main.jpg'\n") :append true)
+    (spit config/galleries-sh (str "cp -v '" image-name "&main.jpg' " "joomgallery/details/" path "/" image-name ".jpg" "\n") :append true)
+    (spit config/galleries-sh (str "cp -v '" image-name "&main.jpg' " "joomgallery/originals/" path "/" image-name ".jpg" "\n") :append true)
+    (spit config/galleries-sh (str "mogrify -verbose -path joomgallery/thumbnails/" path " -thumbnail 160x160 joomgallery/details/" path "/" image-name ".jpg" "\n") :append true)
     (str "INSERT INTO `"
          config/table-prefix
          "joomgallery` (`id`, `asset_id`, `catid`, `imgtitle`, `alias`, `imgauthor`, `imgtext`, `imgdate`, `imgfilename`, `imgthumbname`, `published`, `approved`, `ordering`, `access`, `params`, `metakey`, `metadesc`) VALUES ("
-         (get-asset-id) ","
          (get-image-id) ","
+         (get-asset-id) ","
          gallery-id ","
-         "'" (s/replace (s/replace (.toLowerCase (:title image-map)) #" " "_") #"'" "_") "'" ","
          "'" (s/replace (:title image-map) #"'" "''") "'" ","
-         "'" (:_sown_ image-map) "'" ","
+         "'" (s/replace (s/replace (.toLowerCase (:title image-map)) #" " "-") #"'" "-") "'" ","
+         "'" (filter-owner (:_sown_ image-map)) "'" ","
          ;;         "'" (:description image-map) "'" ","
          "'" (Jsoup/clean (s/replace (s/replace (:description image-map) #"'" "''") #"\\" "") (Whitelist.)) "'" ","
          "'" (utils/creation-time (clojure.java.io/file (str "resources/users_veramente/images/" image-name ".ffc"))) "'" ","
@@ -78,12 +89,13 @@
         (str sql
              "INSERT INTO `"
              config/table-prefix
-             "joomgallery_catg` (`cid`, `name`, `alias`, `parent_id`, `lft`, `asset_id`, `level`, `published`, `catpath`, `access`, `params`, `metakey`, `metadesc`, `exclude_toplists`, `exclude_search`) VALUES ("
+             "joomgallery_catg` (`cid`, `name`, `alias`, `parent_id`, `lft`, `rgt`, `asset_id`, `level`, `published`, `catpath`, `access`, `params`, `metakey`, `metadesc`, `exclude_toplists`, `exclude_search`) VALUES ("
              (key gallery) ","
              "'" (s/replace (.toLowerCase (:title (val gallery))) #" " "_") "'" ","
              "'" (:title (val gallery)) "'" ","
              "1" ","
-             "1" ","
+             (get-lft-id) ","
+             (get-rgt-id) ","
              (get-asset-id) ","
              "1" ","
              "1" ","
@@ -95,8 +107,9 @@
              "0" ","
              "0" ");\n"
              )]
-    (spit "/home/saidone/Vagrant VMs/veramente-deb/copy-images.sh" (str "mkdir -p joomgallery/details/" (s/replace (.toLowerCase (:title (val gallery))) #" " "_") "\n") :append true)
-    (spit "/home/saidone/Vagrant VMs/veramente-deb/copy-images.sh" (str "mkdir -p joomgallery/thumbnails/" (s/replace (.toLowerCase (:title (val gallery))) #" " "_") "\n") :append true)
+    (spit config/galleries-sh (str "mkdir -v -p joomgallery/details/" (s/replace (.toLowerCase (:title (val gallery))) #" " "_") "\n") :append true)
+    (spit config/galleries-sh (str "mkdir -v -p joomgallery/originals/" (s/replace (.toLowerCase (:title (val gallery))) #" " "_") "\n") :append true)
+    (spit config/galleries-sh (str "mkdir -v -p joomgallery/thumbnails/" (s/replace (.toLowerCase (:title (val gallery))) #" " "_") "\n") :append true)
     (str sql
          (apply str
                 (do
@@ -105,14 +118,18 @@
                    (partial create-image (s/replace (.toLowerCase (:title (val gallery))) #" " "_") (key gallery))
                    (:content (val gallery))))))))
 
-(spit "/home/saidone/Vagrant VMs/veramente-deb/galleries.sql"
-      (let [galleries
-            (reduce
-             parse-gallery
-             {}
-             (take 2 config/galleries))]
-        (reduce
-         create-gallery
-         ""
-         galleries)))
+(spit config/galleries-sh "")
 
+(spit config/galleries-sql-output
+      (do (reset! gallery-id 1)
+          (reset! image-id 1)
+          (reset! asset-id 9000)
+          (let [galleries
+                (reduce
+                 parse-gallery
+                 {}
+                 config/galleries)]
+            (reduce
+             create-gallery
+             ""
+             galleries))))
